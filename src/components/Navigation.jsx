4d954c9486a735f86e98ea7bbb73e868fc8434ed
martinsetaro <lordsesony@gmail.com>
2023-10-09 18:28:49 -0300

    arreglo de rutas del navegador

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -22,9 +22,9 @@ const handlerLoad = () => {
     <div className="w-full h-[8rem] text-white uppercase gap-12 font-nunito flex items-center justify-center max-[600px]:hidden">
         <Link href="/" className="link">Inicio</Link>
         <Link href="/Turnos" className="link">Turnos</Link>
-        <Link href="#nuestros" className="link">Trabajos</Link>
+        <Link href="/#nuestros" className="link">Trabajos</Link>
     </div>
-    <div className=" max-[600px]:w-full h-[6rem] max-[600px]:flex uppercase max-[600px]:justify-center max-[600px]:items-center ">
+    <div className="min-[601px]:hidden max-[600px]:w-full h-[6rem] max-[600px]:flex uppercase max-[600px]:justify-center max-[600px]:items-center ">
       <h2 className="text-3xl text-white cursor-pointer -mb-0"
       onClick={handlerLoad}
       >Menu</h2>
@@ -32,7 +32,7 @@ const handlerLoad = () => {
    { !load && <div className="max-[600px]:w-full h-auto bg-gray-500 flex flex-col justify-center gap-3 text-white text-center pt-2 pb-2">
         <Link href="/" className="link">Inicio</Link>
         <Link href="/Turnos" className="link">Turnos</Link>
-        <Link href="#nuestros" className="link">Trabajos</Link>
+        <Link href="/#nuestros" className="link">Trabajos</Link>
    </div>}
     
     
