5abe93daac979d417b21ac2bc8a70db428e423af
martinsetaro <lordsesony@gmail.com>
2023-10-09 15:13:38 -0300

    agregue formulario y mensaje a whatsapp

--- src/app/Turnos/page.js
+++ src/app/Turnos/page.js
@@ -1,10 +1,51 @@
 "use client";
 import { useState } from "react";
-import Header from "@/components/Header";
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
 
 
 
 const page = () => {
+
+  const [nombre,setNombre] = useState('')
+  const [apellido,setApellido] = useState('')
+  const [telefono,setTelefono] = useState('')
+  const [turno,setTurno]= useState('')
+  const [direccion,setDireccion] = useState('')
+  const [dia,setDia] = useState('')
+
+
+
+  const handlerSolicitar=(e)=> {
+ e.preventDefault();
+
+
+  if([nombre,apellido,telefono,turno,direccion,dia].includes('')){
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Encontre algunos campos vacios!',
+      
+    })}
+    else {
+      window.location.href=`https://api.whatsapp.com/send?phone=+542617228662&text=Mi%20nombre%20es%20${nombre}%20y%20solicito%20un%20turno%20para%20el%20dia%20${dia}%20a%20las%20${turno}%20,%20mi%20telefono%20es%20${telefono}.%20Muchas%20gracias!`
+    }
+  }
+   
+// Generar opciones para los días del mes (01 al 31)
+const days = Array.from({ length: 31 }, (_, index) => {
+  const day = (index + 1).toString().padStart(2, '0'); // Agrega el cero inicial si es necesario
+  return <option key={day} value={day}>{day}</option>;
+});
+
+
+  
+
+  
+
+
+
+  
 
   return (
     
@@ -17,23 +58,58 @@ const page = () => {
           <input
           type="text"
           placeholder="Ingrese su nombre"
-          className="pl-2 border-[.1rem] rounded-sm pt-2 pb-2"
+          className="pl-2 border-[.1rem] rounded-sm -mt-[1rem]"
+          onChange={(e)=> setNombre(e.target.value)}
           />
           <label>Apellido</label>
           <input
           type="text"
           placeholder="Ingrese su apellido"
-          className="pl-2 border-[.1rem] rounded-sm pt-2 pb-2"
+          className="pl-2 border-[.1rem] rounded-sm -mt-[1rem]"
+          onChange={(e)=> setApellido(e.target.value)}
           />
           <label>Telefono</label>
           <input
           type="number"
           placeholder="Ingrese su telefono"
-          className="pl-2 border-[.1rem] rounded-sm pt-2 pb-2"
+          className="pl-2 border-[.1rem] rounded-sm -mt-[1rem]"
           min='0'
+          onChange={(e)=> setTelefono(e.target.value)}
           />
+          <label>Dirección</label>
+          <input
+          type="text"
+          placeholder="Ingrese su dirección"
+          className="pl-2 border-[.1rem] rounded-sm -mt-[1rem]"
+          onChange={(e)=> setDireccion(e.target.value)}
+          />
+          <label>Seleccione su horario:</label>
+          <select 
+          className="pl-2 border-[.1rem] rounded-sm -mt-[1rem]"
+          onChange={(e)=> setTurno(e.target.value)}
+          >
+            <option value="" >Elegir turno</option>
+            <option value="14:00">14:00</option>
+            <option value="15:00">15:00</option>
+            <option value="16:00">16:00</option>
+            <option value="17:00">17:00</option>
+            <option value="18:00">18:00</option>
+            <option value="19:00">19:00</option>
+            <option value="20:00">20:00</option>
+            <option value="21:00">21:00</option>
+          </select>
+          <label>Seleccione el dia</label>
+          <select 
+          className="pl-2 border-[.1rem] rounded-sm -mt-[1rem]"
+          onChange={(e)=> setDia(e.target.value)}
+          id="day" name="day"
+          >
+            <option value="">Elegir dia</option>
+            {days}
+          </select>
           <button
           className="bg-gray-900 text-white font-nunito w-[10rem] h-[3rem] rounded-md"
+          onClick={handlerSolicitar}
           >
           Solicitar Turno
           </button>
